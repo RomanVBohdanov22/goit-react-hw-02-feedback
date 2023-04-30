1bf0442b58abf82c7f1df65b9cc3940f31550db8
RomanVBohdanov22 <113215473+RomanVBohdanov22@users.noreply.github.com>
2023-04-30 19:32:50 +0300

    lets do 02

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -46,7 +46,7 @@ export class App extends Component {
         <div>
           <Section title="Please leave feedback">
           <FeedbackOptions
-            options={Object.keys(this.state)}//['good', 'neutral', 'bad']
+            options={Object.keys(this.state)}
             onLeaveFeedback={this.onLeaveFeedback}
             />
           </Section>
